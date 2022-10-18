0c39d901bc1c19d411d24d4c54ed5f34668d8cd0
darkoverlordofdata <darkoverlordofdata@gmail.com>
2022-10-18 00:00:00 -0400

    update

---
+++ Converter/Convertor_main.m
@@ -0,0 +1,30 @@
+/* 
+   Project: Convertor
+
+   Author: bruce davidson
+
+   Created: 2022-10-17 18:33:11 -0400 by darko
+*/
+
+#import <AppKit/AppKit.h>
+
+int 
+main(int argc, const char *argv[])
+{
+// Uncomment if your application is Renaissance application
+/*  CREATE_AUTORELEASE_POOL (pool);
+  [NSApplication sharedApplication];
+  [NSApp setDelegate: [AppController new]];
+
+  #ifdef GNUSTEP
+    [NSBundle loadGSMarkupNamed: @"MainMenu-GNUstep"  owner: [NSApp delegate]];
+  #else
+    [NSBundle loadGSMarkupNamed: @"MainMenu-OSX"  owner: [NSApp delegate]];
+  #endif
+   
+  RELEASE (pool);
+*/
+
+  return NSApplicationMain (argc, argv);
+}
+
